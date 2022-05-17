8c18def8b9ed4c0ec4d8aa8afabf31bd92ceedda
phcxc <h499871809@gmail.com>
2022-05-17 18:09:44 +0800

    feat: use ISO format

--- src/tracker.ts
+++ src/tracker.ts
@@ -146,7 +146,7 @@ export class Tracker {
   }
 
   async stop() {
-    this.logger.info(`[Tracker] Stopping worker for chain at: ${new Date()}`);
+    this.logger.info(`[Tracker] Stopping worker for chain at: ${new Date().toISOString()}`);
     this.stopped = true;
     this.isSyncing = false;
   }
@@ -181,7 +181,7 @@ export class Tracker {
       if (rollback.rollback) {
         if (!rollback.synced || !rollback.remote) throw new Error('rollback synced or remote is undefined');
         this._currentBlock = await this.doRollback(rollback.synced, rollback.remote);
-        this.logger.info(`[Tracker] Rollback... rollback ${rollback.synced}, current ${this.currentBlock}, latest ${this.remoteBlock} at ${new Date()}`);
+        this.logger.info(`[Tracker] Rollback... rollback ${rollback.synced}, current ${this.currentBlock}, latest ${this.remoteBlock} at ${new Date().toISOString()}`);
         this.isSyncing = false;
         return;
       }
@@ -196,13 +196,13 @@ export class Tracker {
         return;
       }
 
-      this.logger.info(`[Tracker] Ing... ${this.currentBlock.number} -> ${this.remoteBlock.number} behind ${distance}, will sync ${needed} blocks at ${new Date()}`);
+      this.logger.info(`[Tracker] Ing... ${this.currentBlock.number} -> ${this.remoteBlock.number} behind ${distance}, will sync ${needed} blocks at ${new Date().toISOString()}`);
 
       this._currentBlock = await this.succeeded(this.currentBlock, needed);
       this.isSyncing = false;
       return;
     } catch (e: any) {
-      this.logger?.error(e, `[Tracker] failed: height ${this.currentBlock.number} at: ${new Date()}`);
+      this.logger?.error(e, `[Tracker] failed: height ${this.currentBlock.number} at: ${new Date().toISOString()}`);
       await this.failed(this.currentBlock);
       this.sleep(this.interval);
       this.isSyncing = false;
